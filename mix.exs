b7ca793413dc0a2c5629b40d56ee79482c9baf5c
PavelTchoff <zineditchoffo@Zinedis-MacBook-Pro.local>
2023-11-08 11:34:30 +0100

    debug: cleaaning crypto deps

--- mix.exs
+++ mix.exs
@@ -51,7 +51,7 @@ defmodule Timemanager.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:plug_cowboy, "~> 2.5"},
-      {:bcrypt_elixir, "3.1.0"},
+      {:bcrypt_elixir, "~> 3.0"},
       {:json_web_token, "0.2.10"},
       {:corsica, "~> 2.0"},
       {:poison, "~> 3.1"},
